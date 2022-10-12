0cebf5b29287ec0418c3bba01a61491f2557266a
omarbabou <obabou50@gmail.com>
2022-10-12 15:00:57 +0200

    Add capitalization and trim methods

--- src/person.rb
+++ src/person.rb
@@ -1,9 +1,12 @@
-class Person
+require_relative './decorate'
+
+class Person < Decorate
   def initialize(age, name = 'Unknown', parent_permission: false)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_reader :id
@@ -17,5 +20,9 @@ class Person
 
   def can_use_services?
     is_of_age || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end

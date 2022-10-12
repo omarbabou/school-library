c49b1882bca277af522ef46b5bfa90e8e4b0e425
omarbabou <obabou50@gmail.com>
2022-10-12 14:56:05 +0200

    create the decorator, trimerDecorator and the catipatilizeDecorator c…

--- src/decorate.rb
+++ src/decorate.rb
@@ -15,3 +15,15 @@ class Nameable
     end
   end
   
+  class CapitalizeDecorator < Decorator
+    def correct_name
+      @nameable.correct_name.capitalize
+    end
+  end
+  
+  class TrimmerDecorator < Decorator
+    def correct_name
+      @nameable.correct_name.split.first(10).join(' ')
+    end
+  end
+  

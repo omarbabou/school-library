4a3978c530857075dc47e066360d6d95f28b9cfb
omarbabou <obabou50@gmail.com>
2022-10-13 17:49:56 +0200

    Add changes required from reviewr

--- src/app.rb
+++ src/app.rb
@@ -1,9 +1,9 @@
-require './book'
-require './classroom'
-require './person'
-require './rental'
-require './student'
-require './teacher'
+require_relative './person'
+require_relative './student'
+require_relative './teacher'
+require_relative './book'
+require_relative './classroom'
+require_relative './rentals'
 
 class App
   def initialize
@@ -12,111 +12,118 @@ class App
     @rentals = []
   end
 
-  def all_booklist
-    puts 'There is no book found in the store' if
-    @books.empty?
-    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+  def entry_point
+    puts 'Welcome to School Library App!'
+
+    until all_options
+      answer = gets.chomp.to_i
+      if answer == 7
+        puts 'Thank you for choosing this App'
+        break
+      end
+      user_input answer
+    end
   end
 
-  def all_peoplelist
-    puts 'No people found!' if @people.empty?
-    @people.each { |person| puts "[#{person.class}], Age: #{person.age}, Name: #{person.name}, Id: #{person.id}" }
+  def list_all_books
+    puts 'Database is empty! Add a book.' if @books.empty?
+    @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
+  end
+
+  def list_all_people
+    puts 'Database is empty! Add a person.' if @people.empty?
+    @people.each do |person|
+      puts "[#{person.class.name}] Name: #{person.name}, Age: #{person.age}, id: #{person.id}"
+    end
   end
 
   def create_person
-    print 'Missing student or teacher? create student (1) or a teacher (2)
-        [Input the number]: '
+    print 'To create a student, press 1, to create a teacher, press 2 : '
     option = gets.chomp
+
     case option
     when '1'
       create_student
     when '2'
       create_teacher
     else
-      puts 'invalid input'
+      puts 'Invalid input. Try again'
     end
-    puts 'Person created successfully'
-  end
-
-  def create_teacher
-    print 'teacher age: '
-    age = gets.chomp
-
-    print 'teacher specialization: '
-    specialization = gets.chomp
-
-    print 'teacher name: '
-    name = gets.chomp
-
-    teacher = Teacher.new(age, specialization, name, parent_permission: true)
-    puts 'New teacher was created succefully'
-    @people.push(teacher)
   end
 
   def create_student
-    print 'student age: '
-    age = gets.chomp
-
-    print 'student name: '
+    puts 'Create a new student'
+    print 'Enter student age: '
+    age = gets.chomp.to_i
+    print 'Enter name: '
     name = gets.chomp
-
-    print 'student classroom: '
-    classroom = gets.chomp
-
-    case classroom
-    when 'no'
-      student = Student.new(age, classroom, name, parent_permission: false)
-      @people.push(student)
-    when 'yes'
-      student = Student.new(age, classroom, name, parent_permission: true)
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase
+    case parent_permission
+    when 'n'
+      puts 'Student does not have parent permission, can not rent books'
+    when 'y'
+      new_student = Student.new(age, name, parent_permission: false)
+      @people << new_student
+      puts 'Student created successfully'
     end
-    puts 'New student was created successfully'
   end
 
-  def create_book
-    print 'book title: '
+  def create_teacher
+    puts 'Create a new teacher'
+    print 'Enter teacher age: '
+    age = gets.chomp.to_i
+    print 'Enter teacher name: '
+    name = gets.chomp
+    print 'Enter teacher specialization: '
+    specialization = gets.chomp
+    @people.push(Teacher.new(age, specialization, name))
+    puts 'Teacher created successfully'
+  end
+
+  def create_book()
+    puts 'Create a new book'
+    print 'Enter title: '
     title = gets.chomp
-
-    print 'book author: '
-    author = gets.chomp
-
-    book = Book.new(title, author)
-    @books.push(book)
-
-    puts 'New book item was successfully created'
+    print 'Enter author: '
+    author = gets
+    @books.push(Book.new(title, author))
+    puts "Book #{title} created successfully."
   end
 
   def create_rental
-    if @books.size.zero?
-      puts 'Books not found'
-    elsif @people.size.zero?
-      puts 'Person Not found'
-    else
-      puts 'select book item by number'
-      @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-      selected_book = gets.chomp.to_i
+    puts 'Select which book you want to rent by entering its number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
 
-      puts 'select a person by number'
-      @people.each_with_index do |person, index|
-        puts "#{index}) Name: #{person.name} Age: #{person.age} Id: #{person.id}"
-      end
-      selected_person = gets.chomp.to_i
+    book_id = gets.chomp.to_i
 
-      puts 'Enter date [YYYY-MM-DD]'
-      date = gets.chomp.to_s
-
-      rental_item = Rental.new(date, @books[selected_book], @people[selected_person])
-      @rentals.push(rental_item)
-      puts 'Rental was created successfully'
+    puts 'Select a person from the list by its number'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+
+    person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    @rentals.push(Rental.new(date, @books[book_id], @people[person_id]))
+    puts 'Rental created successfully'
   end
 
-  def rentalslist
-    puts 'inter person id'
+  def list_all_rentals
+    puts 'To see person rentals enter the person ID: '
+    @people.each do |person|
+      puts "id: #{person.id}"
+    end
     id = gets.chomp.to_i
+    puts 'Rented Books:'
     @rentals.each do |rental|
-      if rental.person.id.to_i == id.to_i
-        puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
+      if rental.person.id == id
+        puts "Peson: #{rental.person.name}  Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
+      else
+        puts
+        puts 'No records where found for the given ID'
       end
     end
   end
